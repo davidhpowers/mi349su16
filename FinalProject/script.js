adcdbeb184f81d1b1c10c171a8d642bc6e651f75
David Powers <powers71@cse.msu.edu>
2016-06-30 17:34:15 -0500

    Fixed links, added favicon, removed lines from nav menu, deleted blog links, fixed background image sizing

--- FinalProject/script.js
+++ FinalProject/script.js
@@ -2,105 +2,171 @@ $(document).ready(function(){
 
 	console.log("Hello World");
 
+	console.log($( window ).width());
+
 	var totalYears = 8;
 
 	var startYear = 2008;
 
-	var margin = 20;
+	// Default Margin Value
+	var margin = 15;
+
+	// Extra spacing for dateLine
+	var extra = 13.75;
+
+	if ($( window ).width() < 1000){
+
+		console.log("SIZE: < 1000");
+
+		margin = 5;
+
+		extra = 16;
+
+	}
 
 	// Bracket Array
-	// [start month , start year , end month , end year , short/medium/tall , up/down]
+	// [start month , start year , end month , end year , short/medium/tall , up/down, title, tag]
 	var brackets = [
 		// High School
-		[9, 2008, 6, 2012, "medium", "up"],
+		[9, 2008, 6, 2012, "tall", "up", "Michigan Center High School", "education"],
 
 		// MSU
-		[8, 2012, 12, 2016, "tall", "up"],
+		[8, 2012, 12, 2016, "tall", "up", "Michigan State University", "education"],
 
 		// GM 1st Internship
-		[5, 2014, 8, 2014, "tall", "up"],
+		[5, 2014, 8, 2014, "tall", "up", "GPSC Data Software Development Intern", "work"],
 
 		// GM 2nd Internship
-		[5, 2015, 8, 2015, "tall", "up"],
+		[5, 2015, 8, 2015, "tall", "up", "Global End User Experience SharePoint Development Intern", "work"],
 
 		// GM 3rd Internship
-		[5, 2016, 8, 2016, "tall", "up"],
+		[5, 2016, 8, 2016, "tall", "up", "GCCX Onstar Web Development Intern", "work"],
 
 		// Tru Design Internship
-		[12, 2014, 1, 2015, "medium", "up"],
+		[12, 2014, 1, 2015, "medium", "up", "Tru Design Home Automation Intern", "work"],
 
 		// University Activities Board
-		[1, 2013, 5, 2013, "short", "up"],
+		[1, 2013, 5, 2013, "short", "up", "University Activities Board Member", "extra-curricular"],
 
 		// Big Brothers Big Sisters
-		[8, 2011, 6, 2012, "short", "up"],
+		[8, 2011, 6, 2012, "short", "down", "Big Brothers Big Sisters", "volunteer"],
 
 		// National Honor Society
-		[8, 2010, 6, 2012, "short", "up"],
+		[8, 2010, 6, 2012, "medium", "up", "National Honor Society Member", "volunteer"],
 
 		// FIRST Robotics
-		[12, 2011, 4, 2012, "medium", "up"],
-
-		// Student Council
-		[9, 2008, 6, 2012, "short", "up"],
+		[12, 2011, 4, 2012, "medium", "up", "FIRST Robotics Member", "extra-curricular"],
 
 		// Dave Powers Fence Co.
-		[6, 2011, 8, 2012, "short", "up"],
+		[6, 2011, 8, 2012, "short", "down", "Dave Powers Fence Co. Technology Manager", "work"],
 
 		// Audio Enthusiasts & Engineers
-		[1, 2015, 5, 2015, "short", "up"],
+		[1, 2015, 5, 2015, "short", "up", "Audio Enthusiasts & Engineers Member", "extra-curricular"],
 
 		// Student Representative on School Board
-		[9, 2011, 5, 2012, "short", "up"],
+		[9, 2011, 5, 2012, "short", "up", "Student Representative on the Michigan Center School Board", "extra-curricular"],
 
 		// Lyle A. Torrant Center Volunteer
-		[8, 2011, 5, 2012, "short", "up"],
+		[8, 2011, 5, 2012, "short", "up", "Lyle A. Torrant Center Volunteer", "volunteer"],
 
 		// Varsity Cross Country & Track - Team Captain & Academic All Conference
-		[8, 2008, 6, 2012, "short", "up"],
+		[9, 2008, 6, 2012, "medium", "down", "Varsity Cross Country & Track Team Captain", "extra-curricular"],
 
 		// MSU Detroit Semester
-		[5, 2015, 8, 2015, "short", "up"]
+		[5, 2015, 8, 2015, "short", "up", "MSU Detroit Semester", "volunteer"]
 	];
 
 	// Event Array
-	// [start month , start year]
+	// [start month , start year, title, tag]
 	var events = [
-		[6, 2012],
-		[8, 2012],
-		[12, 2016]
+		[9, 2008, "Started at Michigan Center High School", "education"],
+		[6, 2012, "Graduated from Michigan Center High School", "education"],
+		[8, 2012, "Started at Michigan State University", "education"],
+		[12, 2016, "Graduation from Michigan State University", "education"]
 	];
 
+	//timeLine(totalYears, startYear, margin, extra);
 
-	timeLine(totalYears, startYear, margin);
+	//enterBrackets(brackets);
 
-	enterBrackets(brackets);
+	//enterEvents(events);
 
-	enterEvents(events);
+	//hideShow(brackets, events);
 
 });
 
 $(document).scroll(function() {
-	if ($(document).scrollTop() > 0) {
+	console.log("scroll");
+	if ($(document).scrollTop() >= $("#profile").height() - 30) {
 		// user scrolled 50 pixels or more;
 		// do stuff
-		$("#menuBar").addClass("black");
+		$("#menuBarNav").show();
 	} else {
-		$("#menuBar").removeClass("black");
+		$("#menuBarNav").hide();
 	}
 });
 
+function hideShow(brackets, events) {
+	$("input[value='All']").click(function() {
+
+		$(".event").show();
+		$(".bracket").show();
+	});
+
+	$("input[value='Education']").click(function() {
+
+		$(".bracket").hide();
+		$(".event").hide();
+
+		$(".bracket.education").show();
+		$(".event.education").show();
+
+	});
+
+	$("input[value='WorkExperience']").click(function() {
+
+		$(".bracket").hide();
+		$(".event").hide();
+
+		$(".bracket.work").show();
+
+	});
+
+	$("input[value='Volunteering']").click(function() {
+
+		$(".event").hide();
+		$(".bracket").hide();
+
+		$(".bracket.volunteer").show();
+
+		$(".bracket.extra-curricular").show();
+	});
+
+	/*
+	 $("input[value='ExtraCurricular']").click(function() {
+
+	 $(".event").hide();
+	 $(".bracket").hide();
+
+	 $(".bracket.extra-curricular").show();
+
+	 });
+	 */
+
+	$("input:radio:first").prop("checked", true).trigger("click");
+}
 
 function enterEvents(events){
 	var count = 1;
 
 	$.each(events, function (index, value) {
-		console.log(value);
+		//console.log(value);
 		$( ".events" ).append('<div id="event'+ count +'" class="event"></div>');
+		$( "#event" + count ).addClass(value[3]);
 
 		var position1 = $("#"+value[0]+"-"+value[1]).position();
 
-		console.log("position1: ", position1);
+		//console.log("position1: ", position1);
 
 		$( "#event"+ count ).css({"left": position1.left + "px"});
 
@@ -114,25 +180,30 @@ function enterBrackets(brackets){
 	var count = 1;
 
 	$.each(brackets, function (index, value) {
-		console.log(value);
-		$( ".brackets" ).append('<div id="bracket'+ count +'" class="bracket '+ value[4] + " " + value[5] + ' "></div>');
+		//console.log(value);
+		$( ".brackets" ).append('<div id="bracket'+ count +'"></div>');
+
+		$( "#bracket" + count ).addClass("bracket");
+		$( "#bracket" + count ).addClass(value[4]);
+		$( "#bracket" + count ).addClass(value[5]);
+		$( "#bracket" + count ).addClass(value[7]);
+
+		$( "#bracket" + count ).append(value[6]);
 
 		var position1 = $("#"+value[0]+"-"+value[1]).position();
 
 		var position2 = $("#"+value[2]+"-"+value[3]).position();
 
-		console.log("position1: ", position1);
-		console.log("position2: ", position2);
+		//console.log("position1: ", position1);
+		//console.log("position2: ", position2);
 
 		$( "#bracket"+ count ).css({"left": position1.left + "px", "width": (position2.left - position1.left) + "px"});
 
 		count++;
 	});
-
-
 }
 
-function timeLine(totalYears, startYear, margin) {
+function timeLine(totalYears, startYear, margin, extra) {
 
 	var year = startYear;
 
@@ -173,14 +244,9 @@ function timeLine(totalYears, startYear, margin) {
 	$( ".dateMarkers" ).append('<div id="'+1+"-"+year+'" class="last dateMarker big"><div class="year"></div></div>');
 	$( ".dateMarker.last" ).css("margin-right", 15 + "px");
 
-	var width = (totalYears + 1) * 13 * margin;
+	var width = (totalYears + 1) * extra * margin;
 
 	$( ".dateLine" ).css({"width": width + "px", "margin-left": -width/2 +"px"});
 
 	$( "#timeLine" ).css({"min-width": width});
-
-
-	//$( ".dateLine" ).css("margin-left", "-" + ($( ".dateLine" ).width()/2) + "px" );
-
-	//console.log("width", ($( ".dateLine" ).width()/2));
 }
